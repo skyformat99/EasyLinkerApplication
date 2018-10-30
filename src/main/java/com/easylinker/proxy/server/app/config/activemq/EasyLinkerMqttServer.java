9a9ecc2c924744cc4d3eeaf632e584ca117feb80
wwhai <751957846@qq.com>
2018-10-30 23:36:26 +0800

    修改代码

--- src/main/java/com/easylinker/proxy/server/app/config/activemq/EasyLinkerMqttServer.java
+++ src/main/java/com/easylinker/proxy/server/app/config/activemq/EasyLinkerMqttServer.java
@@ -2,6 +2,7 @@ package com.easylinker.proxy.server.app.config.activemq;
 
 import org.apache.activemq.broker.BrokerPlugin;
 import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.broker.TransportConnector;
 import org.apache.activemq.store.PersistenceAdapter;
 import org.apache.activemq.util.IOExceptionHandler;
 import org.slf4j.Logger;
@@ -22,22 +23,6 @@ public class EasyLinkerMqttServer extends BrokerService {
         setPlugins(new BrokerPlugin[]{new AuthPluginInstaller()});
         setAdvisorySupport(false);
         setPersistent(false);
-
-        setIoExceptionHandler(new IOExceptionHandler() {
-            @Override
-            public void handle(IOException e) {
-                System.out.println("异常");
-            }
-
-            @Override
-            public void setBrokerService(BrokerService brokerService) {
-                System.out.println("setBrokerService");
-
-
-            }
-        });
-
-
         setTransportConnectorURIs(new String[]{"mqtt://0.0.0.0:1883?wireFormat.maxInactivityDuration=30000&amp;wireFormat.maxInactivityDurationInitalDelay=10000"});
 
     }
