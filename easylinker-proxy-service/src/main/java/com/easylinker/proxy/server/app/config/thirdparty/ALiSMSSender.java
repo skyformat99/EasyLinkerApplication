09a89a1626906816ed4e1e0c0b973373e214894a
wwhai <751957846@qq.com>
2018-11-15 14:01:24 +0800

    集成短信发送

--- easylinker-proxy-service/src/main/java/com/easylinker/proxy/server/app/config/thirdparty/ALiSMSSender.java
+++ easylinker-proxy-service/src/main/java/com/easylinker/proxy/server/app/config/thirdparty/ALiSMSSender.java
@@ -8,85 +8,54 @@ import com.aliyuncs.exceptions.ClientException;
 import com.aliyuncs.http.MethodType;
 import com.aliyuncs.profile.DefaultProfile;
 import com.aliyuncs.profile.IClientProfile;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class ALiSMSSender {
+    private IAcsClient acsClient;
+    private SendSmsRequest request;
+    @Value("${ali.sms.accessKeyId}")
+    String accessKeyId;
+    @Value("${ali.sms.accessKeySecret}")
+    String accessKeySecret;
+    @Value("${ali.sms.template}")
+    String template;
+    @Value("${ali.sms.signName}")
+    String signName;
 
     /**
-     * @param args
+     * 直接在构造函数中初始化参数
      */
-    public static void main(String[] args) {
-        try {
-            ALiSMSSender smsUtils = new ALiSMSSender();
-            String telphoneString = "18059150204";
-            smsUtils.sendSms(telphoneString);
-        } catch (ClientException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    //随机验证码
-    private int code;
-
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode() {
-        code = (int) (Math.random() * 9999) + 1000;  //每次调用生成一次四位数的随机数
-    }
-
-    //短信API产品名称
-    static final String product = "Dysmsapi";
-    //短信API产品域名
-    static final String domain = "dysmsapi.aliyuncs.com";
-
-    static final String accessKeyId = "LTAIqzQNsdImTdZO";
-    static final String accessKeySecret = "0O4gBo6hOv2RpEDXP44p4kvR9jK3j9";
-
-    public SendSmsResponse sendSms(String telphone) throws ClientException {
-        //设置超时时间
+    public ALiSMSSender() {
         System.setProperty("sun.net.client.defaultConnectTimeout", "10000");
         System.setProperty("sun.net.client.defaultReadTimeout", "10000");
-        //初始化ascClient
         IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
-        DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
-        IAcsClient acsClient = new DefaultAcsClient(profile);
-
-        //组装请求对象
-        SendSmsRequest request = new SendSmsRequest();
-        //使用post提交
+        DefaultProfile.addEndpoint("cn-hangzhou", "Dysmsapi", "dysmsapi.aliyuncs.com");
+        acsClient = new DefaultAcsClient(profile);
+        request = new SendSmsRequest();
         request.setMethod(MethodType.POST);
-        //待发送的手机号
-        request.setPhoneNumbers(telphone);
-        //短信签名
-        request.setSignName("爱卓网络科技");
-        //短信模板ID
-        request.setTemplateCode("SMS_14235739");
-        //验证码
-        ALiSMSSender sms = new ALiSMSSender();
-        sms.setCode();
-        String codetemp = sms.getCode() + "";
-        System.out.println("code:        " + codetemp);
-        /*
-         * 可选:模板中的变量替换JSON串,
-         * 如模板内容为"亲爱的${name},您的验证码为${code}"时,
-         * 此处的值为{"name":"Tom","code":"1454"}
-         *   \  反斜杠为转义字符，使得输出双引号
-         */
-        request.setTemplateParam("{\"name\":\"Tom\", \"code\":\"" + codetemp + "\"}");
-        //可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
-        //request.setOutId("1454");
-        SendSmsResponse response = acsClient.getAcsResponse(request);
+        request.setSignName(signName);
+        request.setTemplateCode(template);
 
+    }
+
+//
+//    static final String accessKeyId = "LTAIqzQNsdImTdZO";
+//    static final String accessKeySecret = "0O4gBo6hOv2RpEDXP44p4kvR9jK3j9";
+//    String template = "SMS_14235739";
+//    String signName = "爱卓网络科技";
+    public boolean sendSms(String to, String code) throws ClientException {
+        request.setPhoneNumbers(to);
+        request.setTemplateParam("{\"code\":\" " + code + " \", \"product\": \"EasyLinker\"}");
+        SendSmsResponse response = acsClient.getAcsResponse(request);
         if (response.getCode() != null && response.getCode().equals("OK")) {
-            //请求成功
-            System.out.println("发送成功！");
+            System.out.println("发送成功！" + "key:" + to + " Value:" + code);
+            return true;
         } else {
-            System.out.println("发送失败！");
+            System.out.println("发送失败！" + "key:" + to + " Value:" + code);
+            return false;
         }
-        return response;
     }
 }
 
